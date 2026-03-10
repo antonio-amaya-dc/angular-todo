287cd9095b1bae073bbcd908c8f7dfae4267bb95
antonio-amaya-dc <jamaya@doblecinco.ai>
2026-03-10 17:00:25 -0600

    chore: se realizan modificaciones para probar el consumo y el ngfor

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,19 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone:true,
+  imports: [RouterOutlet,CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('todoapp');
+  welcome = 'todoapp';
+  tasks = [
+    'Instalar Angular CLI',
+    'Crear proyecto',
+    'Crear Lista'
+  ];
 }
